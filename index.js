877cb7f8f39224ce80cb7ecf5937ab41c672df7e
wbabello <wbabello@yahoo.com>
2021-08-04 21:46:54 +0100

    saved

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var movies = [
   "13TH: A Conversation with Oprah Winfrey & Ava DuVernay , March 8th, 2019",
   "18 Presents , April 1st, 2021",
   "1BR  February 2nd, 2020",
-  "100 Meters December 8th, 2021"
+  "100 Meters December 8th, 2021",
 ];
 // document.querySelector(".schedule").innerHTML = movies[0];
 for (let i = 0; i < movies.length; i++) {
@@ -24,3 +24,25 @@ for (let i = 0; i < movies.length; i++) {
 
 const random = Math.floor(Math.random() * 9);
 document.querySelector(".random").innerHTML += `<p>${movies[random]}</p>`;
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
