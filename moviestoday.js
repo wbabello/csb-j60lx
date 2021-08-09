93fb1557f772d4c2c0c35cd840a382fd7e1fdb65
wbabello <wbabello@yahoo.com>
2021-08-09 08:36:00 +0100

    updated

--- moviestoday.js
+++ moviestoday.js
@@ -1,7 +1,11 @@
 var movies = [
   "1898: Our Last Men in the Philippines , June 13th, 2021",
   "13TH: A Conversation with Oprah Winfrey & Ava DuVernay , June 14th, 2021",
-  "100 Meters June 15th,  2021"
+  "100 Meters June 15th,  2021",
+  "Fahrenheit 9/11 April 18, 2004",
+  "Private Life September 13, 2017",
+  "The Hurt Locker October 7, 2021"
+
 ];
 
 for (let i = 0; i < movies.length; i++) {
