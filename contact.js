7bc4fbd595e672ed5ffe1c6d40f6d022063c4d38
wbabello <wbabello@yahoo.com>
2021-08-09 07:51:53 +0100

    Updated

---
+++ contact.js
@@ -0,0 +1,13 @@
+
+
+
+const checkbox = document.querySelector('.my-form input[type="checkbox"]');
+const btns = document.querySelectorAll(".my-form button");
+
+checkbox.addEventListener("change", function() {
+  const checked = this.checked;
+  for (const btn of btns) {
+    checked ? (btn.disabled = true) : (btn.disabled = false);
+  }
+});
+
